68f198658c96956f0e13407fe377e403bb469cde
Grupposo Anthony <47425003+Nocktiss@users.noreply.github.com>
2019-07-19 08:50:30 +0200

    First Commit
    Add project One Punch Wild

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float speed;
+    public float distanceFromPlayer;
+    public float distanceToAttack;
+
+    private Transform target;
+
+    Animator anim;
+    string walkHash = "skeleton_walk";
+    string attackHash = "skeleton_attack";
+    string idleHash = "skeleton_idle";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( (target.transform.position - transform.position).sqrMagnitude < distanceFromPlayer)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            anim.SetTrigger(walkHash);
+            Debug.Log("STEP 1");
+
+            if ((target.transform.position - transform.position).sqrMagnitude < distanceToAttack)
+            {
+                anim.SetTrigger(attackHash);
+                Debug.Log("STEP 2");
+            }
+        }
+    }
+}
---
+++ Assets/Knight Files/KnightScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightScript : MonoBehaviour
+{
+
+    [SerializeField] float speed = 2f, jumpForce = 500f;
+    Rigidbody2D rb;
+    Animator anim;
+    bool lookRight = true;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float move = Input.GetAxis("Horizontal");
+        float jumpPower = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * move * speed * Time.deltaTime);
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        transform.Translate(Vector2.up * Time.deltaTime * jumpPower);
+        anim.SetFloat("Jump", Mathf.Abs(move));
+
+        if (move > 0 && !lookRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && lookRight)
+        {
+            Flip();
+        }
+
+				///Debug
+				if (Input.GetKeyDown(KeyCode.M)) Wind();
+
+		}
+
+		private void FixedUpdate(){
+			if (Input.GetKeyDown(KeyCode.Space)){
+				rb.AddForce(new Vector2(0, jumpForce));
+			}
+		}
+
+    void Flip()
+    {
+        lookRight = !lookRight;
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+		void Wind(){
+			anim.SetTrigger("Wind");
+		}
+}
